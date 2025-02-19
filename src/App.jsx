4ce47872c4d5ec0ae04058dea3cd24c624faa9a8
Craig Donvito <craigdonvito@MacBookPro.attlocal.net>
2025-02-19 18:18:05 -0500

    Adding in workshop App.jsx code and other steps from workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
+import './App.css'
+import './index.css'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
 
+  console.log('puppyList: ', puppyList);
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  console.log(featuredPup);
+  
   return (
     <>
-      <div>
+      <div className='App'>
+        {
+          puppies.map((puppy) => {
+            return <p onClick={ () => {setFeatPupId(puppy.id)} } key={puppy.id}>{puppy.name}</p>
+          })
+        }
+        
+        {featPupId && (
+        <div className='Details'>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
         
       </div>
       
